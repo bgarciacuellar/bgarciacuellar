910c0684b67720e76175670924d7af90ddf08360
Brayan Garcia Cuellar <bgarciacuellar@outlook.com>
2021-09-24 15:50:49 -0500

    Portfolio complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,10 +72,43 @@ tabs.forEach(tab =>{
 })
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('services__button'),
+      modalCloses = document.querySelector('.services__modal-close')
 
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) =>{
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+// modalCloses.forEach((modalClose) =>{
+//     modalClose.addEventListener('click', () =>{
+//         modalViews.forEach((modalView) =>{
+//             modalView.classList.remove('active-modal')
+//         })
+//     })
+// })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+var swiper = new Swiper('.mySwiper', {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+   
+});
 
 /*==================== TESTIMONIAL ====================*/
 
